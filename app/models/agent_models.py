3586be5052554db971fc881915d33c5104bf0201
AshKuraine <lpc9720@hotmail.com>
2025-12-26 10:50:55 -0500

    login for agent

--- app/models/agent_models.py
+++ app/models/agent_models.py
@@ -10,3 +10,16 @@ class CreateAgentRequest(BaseModel):
     id_empresa: int
     acceso_general: int
     areas_string: str
+
+
+class AgentLoginRequest(BaseModel):
+    """Request model for agent login"""
+    numero_telf: str
+    secret_key: str
+
+
+class AgentLoginResponse(BaseModel):
+    """Response model for agent login"""
+    token: Optional[str] = None
+    id_tipo_mensaje: int
+    mensaje: str
